32a0fa48351855f6641e6d9ae63a6b72d8e7ffe9
nehamarlady <nehamarlady@gmail.com>
2026-02-26 00:03:40 -0800

    variables

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello, I am building a habit tracker");
+let habitName="drink water";
+let streak = 5;
+let completeToday = true;
+let habits =["running","reading","walking"];
+let habit=[{
+    name:"running",
+    streak:3,
+    completeToday: true
+},
+{name:"walking",
+    streak:4,
+    completeToday:false
+
+}]
+console.log(habitName);
+console.log(streak);
+console.log(completeToday);
+console.log(habits);
+console.log(habit)
+console.log(habit[1].streak)
+console.log(habit[0].completeToday)
